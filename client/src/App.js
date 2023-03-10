7bfaf4ce8614cd57d875edfad4ccc478f5f115e5
abhijeetnishal <abhijeetnishal@gmail.com>
2023-03-10 23:20:43 +0530

    Component commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,30 @@ import LandingPage from './components/main/LandingPage'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Footer from './components/footer/Footer'
-//import MyPasswords from './components/main/MyPasswords'
+import Header from './components/header/Header'
+import MyPasswords from './components/main/MyPasswords'
 import SinglePassword from './components/main/SinglePassword'
 import CreatePassword from './components/main/CreatePassword'
 import ProfileScreen from './components/main/ProfileScreen'
 
+import { useState } from "react"
 import { BrowserRouter as Router ,Routes, Route } from "react-router-dom";
 
 function App() {
+  const [search, setSearch] = useState("");
+
   return(
       <Router>
+        <Header setSearch={(s) => setSearch(s)} />
         <Routes>
           <Route path='/' element={<LandingPage/>} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
-          {/* <Route
+          <Route
           path="/mypasswords"
           component={({ history }) => (
             <MyPasswords search={search} history={history} />
-          )}/> */}
+          )}/>
           <Route path="/password/:id" element={<SinglePassword/>} />
           <Route path="/createpassword" element={<CreatePassword/>} />;
           <Route path="/profile" element={<ProfileScreen/>} />
---
+++ client/src/components/main/SinglePassword.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SinglePassword = () => {
+  return (
+    <div>
+        <div>
+            <input type="text" placeholder='enter website name' />
+        </div>  
+        <div>
+            Website name    
+        </div>
+        <div>
+            Password
+        </div>       
+    </div>
+  )
+}
+
+export default SinglePassword
