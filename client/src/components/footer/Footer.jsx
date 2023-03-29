496e4b348178b1249ff53575f71d3b3b387bcda0
Abhijeet Kumar <abhijeetnishal@gmail.com>
2023-03-29 08:26:56 +0530

    working commit

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,12 +3,12 @@ import githubIcon from '../../assets/github-logo.png'
 import linkedinIcon from '../../assets/linkedin.png'
 
 const Footer = () => {
-  return (
+  return ( 
     <div className='footer'>
-      <a href='https://github.com/abhijeetnishal'>
+      <a className='github-link' href='https://github.com/abhijeetnishal'>
         <img className='github-icon' src={githubIcon} alt='' />
       </a>
-      <a href='https://www.linkedin.com/in/abhijeetkumar7565/'>
+      <a className='linkedin-link' href='https://www.linkedin.com/in/abhijeetkumar7565/'>
         <img className='linkedin-icon' src={linkedinIcon} alt='' />
       </a>
     </div>
